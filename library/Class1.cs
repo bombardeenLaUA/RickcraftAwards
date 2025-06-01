459801c77f93bcf4c4fb2f2355b2af1a66beed06
bombardeenLaUA <barcelomcarlos@gmail.com>
2025-06-01 10:08:49 +0200

    añadir el system configuration para el constring

--- library/Class1.cs
+++ library/Class1.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace library
 {
     public class Class1
     {
+        private string constring;
+
+        public Class1()
+        {
+            constring = ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString;
+        }
     }
 }
